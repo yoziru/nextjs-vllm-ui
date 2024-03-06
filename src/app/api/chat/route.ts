c52ceca1b8acc663ae0af97325135fbf41ca3ce9
Yasir Ekinci <yas.ekinci@grafana.com>
2024-03-06 08:34:19 +0100

    chat: format system message role

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -1,7 +1,23 @@
 import { StreamingTextResponse, Message } from "ai";
-import { AIMessage, HumanMessage } from "@langchain/core/messages";
+import {
+  AIMessage,
+  HumanMessage,
+  SystemMessage,
+} from "@langchain/core/messages";
 import { BytesOutputParser } from "@langchain/core/output_parsers";
 import { ChatOpenAI } from "@langchain/openai";
+
+const formatMessages = (messages: Message[]) => {
+  return messages.map((m) => {
+    if (m.role === "system") {
+      return new SystemMessage(m.content);
+    } else if (m.role === "user") {
+      return new HumanMessage(m.content);
+    } else {
+      return new AIMessage(m.content);
+    }
+  });
+};
 
 export async function POST(req: Request) {
   const { messages, selectedModel } = await req.json();
@@ -16,16 +32,7 @@ export async function POST(req: Request) {
   });
 
   const parser = new BytesOutputParser();
-
-  const stream = await model
-    .pipe(parser)
-    .stream(
-      (messages as Message[]).map((m) =>
-        m.role == "user"
-          ? new HumanMessage(m.content)
-          : new AIMessage(m.content)
-      )
-    );
+  const stream = await model.pipe(parser).stream(formatMessages(messages));
 
   return new StreamingTextResponse(stream);
 }
