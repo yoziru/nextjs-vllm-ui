924115a5725d760294ebf6920113e146799e61fa
Yasir Ekinci <yas.ekinci@grafana.com>
2024-03-26 17:23:00 +0100

    make basePath configurable through NEXT_BASEPATH env

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    // set basepath based on environment
+    basePath: process.env.NEXT_BASEPATH ?? "",
     output: "standalone",
 };
 
