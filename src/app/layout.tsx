19b9c229003904443eb7f695d6a71aaf08652622
Yasir Ekinci <yas.ekinci@grafana.com>
2024-03-28 13:30:18 +0100

    remove custom font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,8 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/providers/theme-provider";
 import { Toaster } from "@/components/ui/sonner";
 export const runtime = "edge"; // 'nodejs' (default) | 'edge'
-
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "vLLM UI",
@@ -26,7 +23,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body>
         <ThemeProvider attribute="class" defaultTheme="dark">
           {children}
           <Toaster />
